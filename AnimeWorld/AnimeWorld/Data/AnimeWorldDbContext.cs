bd92ddcbd57fbfc014cb295a1f887986a906386c
KaloyanZulchev <64842320+KaloyanZulchev@users.noreply.github.com>
2021-08-06 16:42:47 +0300

    Create a project

---
+++ AnimeWorld/AnimeWorld/Data/AnimeWorldDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimeWorld.Data
+{
+    public class AnimeWorldDbContext : IdentityDbContext
+    {
+        public AnimeWorldDbContext(DbContextOptions<AnimeWorldDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ AnimeWorld/AnimeWorld/Startup.cs
@@ -0,0 +1,73 @@
+using AnimeWorld.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AnimeWorld
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+            => Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddDbContext<AnimeWorldDbContext>(options => options
+                    .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services
+               .AddDefaultIdentity<IdentityUser>(options =>
+               {
+                   options.Password.RequireDigit = false;
+                   options.Password.RequireLowercase = false;
+                   options.Password.RequireNonAlphanumeric = false;
+                   options.Password.RequireUppercase = false;
+               })
+               .AddRoles<IdentityRole>()
+               .AddEntityFrameworkStores<AnimeWorldDbContext>();
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddControllersWithViews(options =>
+            {
+                options.Filters.Add<AutoValidateAntiforgeryTokenAttribute>();
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app
+               .UseHttpsRedirection()
+               .UseStaticFiles()
+               .UseRouting()
+               .UseAuthentication()
+               .UseAuthorization()
+               .UseEndpoints(endpoints =>
+               {
+                   endpoints.MapDefaultControllerRoute();
+                   endpoints.MapRazorPages();
+               });
+        }
+    }
+}
